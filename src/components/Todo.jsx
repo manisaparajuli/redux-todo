1f718393bfa2b0cc6c1046c2c2431bd028ad2066
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-31 18:26:32 +1000

    complete todo app

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,43 +1,72 @@
-import React, { useState } from 'react'
-import { FaPlus } from "react-icons/fa";
-import { useDispatch } from 'react-redux';
-import { IoSearchSharp } from "react-icons/io5";
+// Todo.js
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TodoList from './TodoList';
+import FilterButton from './FilterButton';
+import { BsSearch, BsPlus } from 'react-icons/bs';
+import { addTodo, updateSearchTerm } from '../redux/actions';
 
 const Todo = () => {
-  const [newTodo, setNewTodo] = useState("")
-  const [searchTerm, setSearchTerm] = useState("")
-  const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  const [newTodoText, setNewTodoText] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const handleAddTodo =(text) =>{
-    dispatch(addTodo(text))
-  }
+  const handleAddTodo = (text) => {
+    dispatch(addTodo(text));
+  };
 
-  const handleAddTodoClick =() =>{
-    if(newTodo.trim() !== ""){
-      handleAddTodo(newTodo.trim())
-      setNewTodo("")
+  const handleAddTodoClick = () => {
+    if (newTodoText.trim() !== '') {
+      handleAddTodo(newTodoText.trim());
+      setNewTodoText('');
     }
-  }
+  };
+
+  const handleSearchChange = (value) => {
+    setSearchTerm(value);
+    dispatch(updateSearchTerm(value));
+  };
 
   return (
-    <div className='max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded'>
-      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase text-purple-800'>Personal todo app</h2>
-      {/* add new task */}
-      <div className='flex items-center mb-4'>
-        <input value={newTodo} type="text" name="addTodoInput" id="addTodoInput" placeholder='Add new task' className='flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500' onChange={(e) => setNewTodo(e.target.value)}/>
-        <button className='ml-4 p-2 bg-sky-300 hover:bg-sky-700 text-white rounded ' onClick={handleAddTodoClick}>< FaPlus  /></button>
+    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
+      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>Personal TODO APP</h2>
+      <div className="flex items-center mb-4">
+        <input
+          id="addTodoInput"
+          className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+          type="text"
+          placeholder="Add Todo"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+        />
+        <button
+          className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          onClick={handleAddTodoClick}
+        >
+          <BsPlus size={20} />
+        </button>
       </div>
 
-      {/* filter and search task */}
-      <div>
-        <div className='flex items-center mb-4'> 
-          <input value={searchTerm} type="text" name="addSearchTerm" id="addSearchTerm" placeholder='Search task' className='flex-end p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500' onChange={(e) => setSearchTerm(e.target.value)}/>
-        <button className='ml-4 p-2 bg-sky-300 hover:bg-sky-700 text-white rounded ' onClick={handleAddTodoClick}>< IoSearchSharp  /></button>
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+        <FilterButton />
+        <div className="flex items-center mb-4">
+          <input
+            className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+            type="text"
+            placeholder="Search Todos"
+            value={searchTerm}
+            onChange={(e) => handleSearchChange(e.target.value)}
+          />
+          <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+            <BsSearch size={20} />
+          </button>
         </div>
       </div>
-
+      <TodoList />
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
